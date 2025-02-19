03472d0d9cde52a4d866f166a90c0e8ec4e72f7c
Abdulrahman Ahmed <abdulrahman.ahmed@scandiweb.com>
2025-02-19 13:39:10 +0200

    updated apollo uri

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -2,12 +2,11 @@ import { ApolloClient, InMemoryCache, from, HttpLink } from '@apollo/client';
 import { onError } from '@apollo/client/link/error';
 
 const httpLink = new HttpLink({
-  uri: 'https://ecommtest.wuaze.com/graphql',
-  credentials: 'include' // try this if you're using cookies
+  uri: 'https://ecommtest.wuaze.com/graphql'
 });
 
 const errorLink = onError(({ graphQLErrors, networkError, operation }) => {
-  console.log('Operation:', operation.operationName); // Log which query/mutation is being attempted
+  console.log('Operation:', operation.operationName);
   if (graphQLErrors) {
     graphQLErrors.forEach(({ message, locations, path }) =>
       console.log(
@@ -17,22 +16,16 @@ const errorLink = onError(({ graphQLErrors, networkError, operation }) => {
   }
   if (networkError) {
     console.log(`[Network error]:`, networkError);
-    // Log more details about the network error
-    console.log('Network Error Details:', {
-      name: networkError.name,
-      message: networkError.message,
-      stack: networkError.stack
-    });
   }
 });
 
 const client = new ApolloClient({
   link: from([errorLink, httpLink]),
   cache: new InMemoryCache(),
+  connectToDevTools: true, // Add this line
   defaultOptions: {
     watchQuery: {
-      fetchPolicy: 'network-only', // Try this to bypass cache
-      nextFetchPolicy: 'cache-first'
+      fetchPolicy: 'network-only'
     }
   }
 });
