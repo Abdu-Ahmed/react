79280c32265c32d836859bfc3a25bb4475adf777
Abdulrahman Ahmed <abdulrahman.ahmed@scandiweb.com>
2025-02-19 12:44:11 +0200

    updated apollo uri

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'ecommtest.wuaze.com/public/graphql',
+  uri: 'https://ecommtest.wuaze.com/',
   cache: new InMemoryCache(),
 });
 
