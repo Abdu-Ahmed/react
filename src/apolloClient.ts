3344a027580708f1da52eafe347c5442d4a1b5bc
Abdulrahman Ahmed <abdulrahman.ahmed@scandiweb.com>
2025-02-19 13:39:17 +0200

    updated apollo uri

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -2,32 +2,26 @@ import { ApolloClient, InMemoryCache, from, HttpLink } from '@apollo/client';
 import { onError } from '@apollo/client/link/error';
 
 const httpLink = new HttpLink({
-  uri: 'https://ecommtest.wuaze.com/graphql'
+  uri: 'https://ecommtest.wuaze.com/graphql',
+  credentials: 'include',
+  headers: {
+    'Content-Type': 'application/json',
+  }
 });
 
-const errorLink = onError(({ graphQLErrors, networkError, operation }) => {
-  console.log('Operation:', operation.operationName);
+const errorLink = onError(({ graphQLErrors, networkError }) => {
   if (graphQLErrors) {
-    graphQLErrors.forEach(({ message, locations, path }) =>
-      console.log(
-        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
-      )
+    graphQLErrors.forEach(({ message }) =>
+      console.log(`[GraphQL error]: Message: ${message}`)
     );
   }
-  if (networkError) {
-    console.log(`[Network error]:`, networkError);
-  }
+  if (networkError) console.log(`[Network error]: ${networkError}`);
 });
 
 const client = new ApolloClient({
   link: from([errorLink, httpLink]),
   cache: new InMemoryCache(),
-  connectToDevTools: true, // Add this line
-  defaultOptions: {
-    watchQuery: {
-      fetchPolicy: 'network-only'
-    }
-  }
+  connectToDevTools: true,
 });
 
 export default client;
