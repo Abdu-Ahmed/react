29096a7576cd1948fba049c1efc8b941fa035e9d
Abdulrahman Ahmed <abdulrahman.ahmed@scandiweb.com>
2025-02-19 13:00:25 +0200

    updated apollo uri

--- src/TestQuery.tsx
+++ src/TestQuery.tsx
@@ -5,6 +5,28 @@ const TEST_QUERY = gql`
     products {
       id
       name
+      inStock
+      gallery
+      description
+      category
+      brand
+      attributes {
+        id
+        name
+        type
+        items {
+          id
+          displayValue
+          value
+        }
+      }
+      prices {
+        amount
+        currency {
+          label
+          symbol
+        }
+      }
     }
   }
 `;
