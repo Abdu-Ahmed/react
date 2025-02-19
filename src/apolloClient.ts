9878698acd9b2294d976a48c25dd512d63232f2c
Abdulrahman Ahmed <abdulrahman.ahmed@scandiweb.com>
2025-02-19 13:29:49 +0200

    updated apollo uri

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -1,20 +1,40 @@
-import { ApolloClient, InMemoryCache, from } from '@apollo/client';
+import { ApolloClient, InMemoryCache, from, HttpLink } from '@apollo/client';
 import { onError } from '@apollo/client/link/error';
 
-const errorLink = onError(({ graphQLErrors, networkError }) => {
-  if (graphQLErrors)
+const httpLink = new HttpLink({
+  uri: 'https://ecommtest.wuaze.com/graphql',
+  credentials: 'include' // try this if you're using cookies
+});
+
+const errorLink = onError(({ graphQLErrors, networkError, operation }) => {
+  console.log('Operation:', operation.operationName); // Log which query/mutation is being attempted
+  if (graphQLErrors) {
     graphQLErrors.forEach(({ message, locations, path }) =>
       console.log(
         `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
       )
     );
-  if (networkError) console.log(`[Network error]: ${networkError}`);
+  }
+  if (networkError) {
+    console.log(`[Network error]:`, networkError);
+    // Log more details about the network error
+    console.log('Network Error Details:', {
+      name: networkError.name,
+      message: networkError.message,
+      stack: networkError.stack
+    });
+  }
 });
 
 const client = new ApolloClient({
-  uri: 'https://ecommtest.wuaze.com/graphql',
+  link: from([errorLink, httpLink]),
   cache: new InMemoryCache(),
-  link: from([errorLink]),
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: 'network-only', // Try this to bypass cache
+      nextFetchPolicy: 'cache-first'
+    }
+  }
 });
 
 export default client;
