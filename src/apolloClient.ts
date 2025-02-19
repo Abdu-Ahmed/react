1b09693fe6b760a9e50a5b697f6c60a677ed9fa1
Abdulrahman Ahmed <abdulrahman.ahmed@scandiweb.com>
2025-02-19 12:09:22 +0200

    Deploy updated react frontend

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:8000',
+  uri: 'ecommtest.wuaze.com',
   cache: new InMemoryCache(),
 });
 
