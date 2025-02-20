bab60fbb231c2abdd702a739f0e27206ad03ac64
Abdulrahman Ahmed <abdulrahman.ahmed@scandiweb.com>
2025-02-20 10:44:42 +0200

    updated apollo uri

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -1,24 +1,32 @@
-import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+import { ApolloClient, InMemoryCache, from, HttpLink } from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
+
+const backendURI =
+  process.env.NODE_ENV === 'production'
+    ? 'https://ecommtest.wuaze.com/graphql'
+    : '/graphql'; // use the proxy in development
 
 const httpLink = new HttpLink({
-  uri: 'https://ecommtest.wuaze.com/graphql',
+  uri: backendURI,
   credentials: 'include',
   headers: {
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json',
+  },
+});
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    graphQLErrors.forEach(({ message }) =>
+      console.log(`[GraphQL error]: Message: ${message}`)
+    );
   }
+  if (networkError) console.log(`[Network error]: ${networkError}`);
 });
 
 const client = new ApolloClient({
-  link: httpLink,
+  link: from([errorLink, httpLink]),
   cache: new InMemoryCache(),
-  defaultOptions: {
-    watchQuery: {
-      fetchPolicy: 'no-cache'
-    },
-    query: {
-      fetchPolicy: 'no-cache'
-    }
-  }
+  connectToDevTools: true,
 });
 
 export default client;
